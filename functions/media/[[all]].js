bb97ba379ecc3f6ba40690f60f7970263ca9bc8f
IZUMI <meng2470769946@gmail.com>
2026-05-16 08:08:36 +0800

    Create [[all]].js

---
+++ functions/media/[[all]].js
@@ -0,0 +1,21 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const filePath = url.pathname.replace('/media/', '');
+  
+  if (!filePath) {
+    return new Response('请指定文件路径', { status: 400 });
+  }
+  
+  try {
+    const object = await env.MY_BUCKET.get(filePath);
+    if (!object) {
+      return new Response('文件不存在', { status: 404 });
+    }
+    return new Response(object.body, {
+      headers: { 'Content-Type': object.httpMetadata?.contentType || 'application/octet-stream' }
+    });
+  } catch (error) {
+    return new Response('读取文件失败: ' + error.message, { status: 500 });
+  }
+}
